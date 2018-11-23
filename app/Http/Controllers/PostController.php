0f6236e58aaaad65855420225eafe1f0b1d741a4
Rogene Cris <rcvioleta13@gmail.com>
2018-11-24 05:45:01 +0800

    added comment, testing stash

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -18,6 +18,7 @@ class PostController extends Controller
     public function index()
     {
         return view('admin.posts.index')->with('posts', Post::all());
+        // added comment
     }
 
     /**
